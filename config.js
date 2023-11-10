0828fadaac95fb4e4d3b5d29f0e9d70e767ad0ea
BrunitoXTania <145606165+BrunitoXTania@users.noreply.github.com>
2023-11-10 13:20:57 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['5212411347465', '𝙾𝙵𝙲 𝚈𝙾𝚅𝙰𝙽𝙸', true],
-  ['5212412377467', '𝙾𝙵𝙲 𝚈𝙾𝚅𝙰𝙽𝙸', true],
-  ['5212411719888', '𝙾𝙵𝙲 𝚈𝙾𝚅𝙰𝙽𝙸', true],
+  ['51906090149', 'OFC BRUNO', true],
+  ['51906090149', 'OFC BRUNO', true],
+  ['51906090149', 'OFC BRUNO', true],
 ]
 
-global.mods = ['5212411719888'],
-global.prems = ['5212411347465'], 
-global.prems = ['5212412377467'],
+global.mods = ['51906090149'],
+global.prems = ['51906090149'], 
+global.prems = ['51906090149'],
 global.APIs = { 
 
   amel: 'https://melcanz.com',
